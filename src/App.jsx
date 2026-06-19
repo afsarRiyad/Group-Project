945f60093cc476f11559cae1003dafcac75d0433
Nurul Afsar Riyad <nurulafsarriyad06@gmail.com>
2026-06-19 17:09:23 +0600

    file setup complete

---
+++ src/App.jsx
@@ -0,0 +1,22 @@
+import Container from "./components/layouts/Container"
+import { Routes, Route } from "react-router";
+import Home from './pages/Home';
+import MainLayout from "./components/layouts/MainLayout";
+
+
+function App() {
+
+  return (
+    <>
+ <Container className="">
+    <Routes>
+     <Route element={<MainLayout/>}>
+        <Route path='/' element={<Home/>} />
+     </Route>
+    </Routes>
+ </Container>
+    </>
+  )
+}
+
+export default App
---
+++ src/components/Navbar.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const Navbar = () => {
+  return (
+    <div className='primaryTxt'>
+      Navbar
+    </div>
+  )
+}
+
+export default Navbar
