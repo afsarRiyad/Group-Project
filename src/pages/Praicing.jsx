e337ebc5d9e87677bf1fba9a98b313eee6a82a77
JANNAT398 <islamjanat956@gmail.com>
2026-06-20 20:40:03 +0600

    praicing done

---
+++ src/pages/Praicing.jsx
@@ -0,0 +1,13 @@
+import React from 'react'
+import Priceplan from '../components/Priceplan'
+import Review from '../components/Review'
+const Praicing = () => {
+  return (
+    <>
+    <Priceplan/>
+    <Review/>
+    </>
+  )
+}
+
+export default Praicing
