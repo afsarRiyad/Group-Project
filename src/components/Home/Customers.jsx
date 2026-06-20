18e491697ea1f74412bded7c49db072c58409dac
Ariyan Akash <ariyanakash01303@gmail.com>
2026-06-20 20:02:57 +0600

    Update

--- src/components/Home/Customers.jsx
+++ src/components/Home/Customers.jsx
@@ -3,6 +3,8 @@ import Container from '../layouts/Container';
 import { FaRegArrowAltCircleRight } from "react-icons/fa";
 import { Link } from 'react-router-dom';
 import manone from '../../assets/images/manone.webp'
+import manTwo from '../../assets/images/mantwo.webp'
+import manthree from '../../assets/images/manthree.webp'
 import { FaArrowRight } from "react-icons/fa6";
 
 const Customers = () => {
@@ -43,7 +45,7 @@ const Customers = () => {
                     </button>
                 </div>
                 <div className='flex flex-col items-start w-full'>
-                    <img src={manone} alt="manone" className='w-full object-cover rounded-lg' />
+                    <img src={manTwo} alt="manTwo" className='w-full object-cover rounded-lg' />
                     <h3 className='headingTxt text-[20px] font-medium mt-6'>Customer Case Study</h3>
                     <p className='primaryTxt leading-6 max-w-[370px] mt-4'>Slate offers a comprehensive suite of tools and features to meet your unique needs.</p>
                     <button className='flex items-center gap-x-2 text-base font-medium mt-[26px] text-black group'>
@@ -52,7 +54,7 @@ const Customers = () => {
                     </button>
                 </div>
                 <div className='flex flex-col items-start w-full'>
-                    <img src={manone} alt="manone" className='w-full object-cover rounded-lg' />
+                    <img src={manthree} alt="manthree" className='w-full object-cover rounded-lg' />
                     <h3 className='headingTxt text-[20px] font-medium mt-6'>Customer Case Study</h3>
                     <p className='primaryTxt leading-6 max-w-[337px] mt-4'>Say goodbye to clunky interfaces and hello to a seamless user experience.</p>
                     <button className='flex items-center mt-[26px] gap-x-2 text-base font-medium text-black group'>
