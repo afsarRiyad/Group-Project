e840a59662a0ceff70dd2eddfe2459ea12041056
Nurul Afsar Riyad <nurulafsarriyad06@gmail.com>
2026-06-20 16:26:22 +0600

    fix navbar

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -7,38 +7,36 @@ import logo from "../assets/images/logo.webp"
 const Navbar = () => {
   return (
        <>
-        <div className=" bg-[#002E25] text-white ">
+        <div className=" bg-[#002E25] text-white font-inter">
          <Container>
-              <div className="flex items-center justify-between py-2.5 px-6">
+              <div className="flex items-center justify-between py-3 px-6">
                 <div className="flex items-center gap-2">
             <HiOutlineCube className="w-4 h-4" />
-            <span>The banner is to inform visitors of an important message</span>
+            <span className=" text-[14px]">The banner is to inform visitors of an important message</span>
               </div>
-          <div className="flex items-center gap-4">
-            <a href="#" aria-label="Facebook" className="hover:opacity-80">
-              <FaFacebookF className="w-4 h-4" />
+          <div className="flex items-center gap-4 ">
+            <a href="#" aria-label="Facebook" className="hover:opacity-80 border border-gray-500 rounded-full p-1">
+              <FaFacebookF className="w-4 h-4 " />
             </a>
-            <a href="#" aria-label="Instagram" className="hover:opacity-80">
+            <a href="#" aria-label="Instagram" className="hover:opacity-80 border border-gray-500 rounded-full p-1 ">
               <FaInstagram className="w-4 h-4" />
             </a>
-            <a href="#" aria-label="Telegram" className="hover:opacity-80">
+            <a href="#" aria-label="Telegram" className="hover:opacity-80 border border-gray-500 rounded-full p-1">
               <FaTelegramPlane className="w-4 h-4" />
             </a>
-            <a href="#" aria-label="Pinterest" className="hover:opacity-80">
-              <FaPinterestP className="w-4 h-4" />
+            <a href="#" aria-label="Pinterest" className="hover:opacity-80 border border-gray-500 rounded-full p-1">
+              <FaPinterestP className="w-4 h-4 " />
             </a>
           </div>
               </div>
          </Container>
         </div>
 
-     <Container className="w-full font-inter">
-      <div className="bg-[#002E25] text-white text-sm">
-      </div>
-
+     <div className=" border-b border-gray-100">
+      <Container className="w-full font-inter">
       {/* Main navigation */}
-      <nav className="bg-white border-b border-gray-100">
-        <div className="flex items-center justify-between px-6 py-6">
+      <nav className="bg-white">
+        <div className="flex items-center justify-between px-6 py-5">
           {/* Logo */}
          <div className="flex gap-18">
             <img src={logo} alt="logo" />
@@ -61,7 +59,7 @@ const Navbar = () => {
          </div>
 
           <div className="flex items-center gap-3">
-            <button className="px-5 py-2.5 text-sm font-medium text-primary border cursor-pointer border-gray-300 rounded-full hover:bg-gray-50 transition-colors">
+            <button className="px-5 py-2.5 text-sm font-medium text-primary border cursor-pointer border-primary rounded-full hover:bg-gray-50 transition-colors">
               Sign in
             </button>
             <button className="px-5 py-2.5 cursor-pointer text-sm font-medium text-white bg-[#002E25] rounded-full hover:bg-[#003d30] transition-colors">
@@ -71,6 +69,7 @@ const Navbar = () => {
         </div>
       </nav>
     </Container>
+     </div>
        </>
   );
 }
