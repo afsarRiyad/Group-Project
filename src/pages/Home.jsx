0bfb63af4f12b69173febcd9c0160133c91d910d
nazmulbd1 <nazmulsheikhnahid@gmail.com>
2026-06-21 15:18:31 +0600

    Nazmul

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,9 +1,21 @@
 import React from 'react'
+import Container from '../components/layouts/Container'
+import SlateDesign from "../assets/images/"
 
 const Home = () => {
   return (
     <>
-       Home
+       <div>
+        <Container>
+          <div className='grid grid-cols-3'>
+            <div>
+              <img src={} alt="" />
+            </div>
+            <div>sfdf</div>
+            <div>sfdfs</div>
+          </div>
+        </Container>
+       </div>
     </>
   )
 }
